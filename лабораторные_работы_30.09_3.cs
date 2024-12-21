1b0bddc2961d263398c0f2ecb8435aeced9db730
prudovik <yaktoblin@gmail.com>
2024-12-22 04:55:11 +0600

    Update лабораторные_работы_30.09_3.cs

--- лабораторные_работы_30.09_3.cs
+++ лабораторные_работы_30.09_3.cs
@@ -5,7 +5,7 @@
 using System;
 class Zadachatri {
   static void Main() {
-      int n, a1, summam = -10000, summa = 0;
+      int n, a1, summam = -1000000, summa = 0;
       n = Convert.ToInt32(Console.ReadLine());
       for(int i = 0; i < n; i++) {
           a1 = Convert.ToInt32(Console.ReadLine());
