eca4c9d04ed49cf030f2f8b93feb542e1adf4b8a
prudovik <yaktoblin@gmail.com>
2024-12-22 04:26:18 +0600

    Update лабораторные_работы_16.09_2.cs

--- лабораторные_работы_16.09_2.cs
+++ лабораторные_работы_16.09_2.cs
@@ -4,7 +4,7 @@
 using System;
 class Zadachadva {
   static void Main() {
-      int a = Convert.ToInt32(Console.ReadLine()), ma = 0, n, ma2 = 0;
+      int a = Convert.ToInt32(Console.ReadLine()), ma = -100000, n, ma2 = -100000;
       for(int i = 1; i <= a; i++) {
           n = Convert.ToInt32(Console.ReadLine());
           if (n > ma) {
